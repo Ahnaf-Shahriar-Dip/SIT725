b8c468691f2502e6583d69407df866d0f259944b
Ahnaf Shahriar Dip <ahnafshahriardip@gmail.com>
2025-07-25 21:37:33 +1000

    update

--- 2.2P/server.js
+++ 2.2P/server.js
@@ -5,17 +5,22 @@ const port = 3000;
 //static files from the public folder
 app.use(express.static('public'));
 
-// GET endpoint
 
 
-// The url will be  http://localhost:3000/hello
+// The url will be for that end point http://localhost:3000/hello
 app.get('/hello', (req, res) => {
   res.send('Hello from the server! I am Ahnaf...');
 });
 
+
+
+
+
+
+
+
+
 // For adding 2 numbers
-
-
 // The url will be for example http://localhost:3000/add?number_1=14&number_2=8
 app.get('/add', (req, res) => {
   const number_1 = parseFloat(req.query.number_1);
